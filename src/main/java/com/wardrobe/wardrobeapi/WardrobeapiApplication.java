915dc630e77333920a64015b2fe5d6b9040cc6a1
Shannon <reidy.pf@gmail.com>
2017-10-12 15:44:13 -0500

    initial commit

---
+++ src/main/java/com/wardrobe/wardrobeapi/WardrobeapiApplication.java
@@ -0,0 +1,12 @@
+package com.wardrobe.wardrobeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardrobeapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wardrobe/wardrobeapi/WardrobeapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wardrobe.wardrobeapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WardrobeapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
