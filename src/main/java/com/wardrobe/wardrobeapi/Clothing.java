149d5e3003cc633aef230ee1756e12e8a8ba5ca2
Shannon <reidy.pf@gmail.com>
2017-10-13 18:28:54 -0500

    added clothing table

---
+++ src/main/java/com/wardrobe/wardrobeapi/Clothing.java
@@ -0,0 +1,34 @@
+package com.wardrobe.wardrobeapi;
+
+public class Clothing {
+    String name;
+    int sleeveLength;
+    int fabricWeight;
+
+    public Clothing() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSleeveLength() {
+        return sleeveLength;
+    }
+
+    public void setSleeveLength(int sleeveLength) {
+        this.sleeveLength = sleeveLength;
+    }
+
+    public int getFabricWeight() {
+        return fabricWeight;
+    }
+
+    public void setFabricWeight(int fabricWeight) {
+        this.fabricWeight = fabricWeight;
+    }
+}
