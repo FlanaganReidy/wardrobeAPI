291ff69603f5c023daf01253cfa637fcdc51190d
Shannon <reidy.pf@gmail.com>
2017-10-17 16:22:24 -0500

    basic CRUD added

--- src/main/java/com/wardrobe/wardrobeapi/controller/WardrobeController.java
+++ src/main/java/com/wardrobe/wardrobeapi/controller/WardrobeController.java
@@ -1,8 +1,10 @@
 package com.wardrobe.wardrobeapi.controller;
 
 import com.wardrobe.wardrobeapi.models.Clothing;
+import com.wardrobe.wardrobeapi.repository.WardrobeRepository;
 import com.wardrobe.wardrobeapi.service.WardrobeService;
 import com.wardrobe.wardrobeapi.service.WardrobeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,26 @@ import java.util.List;
 @RestController
 public class WardrobeController {
 
-    @RequestMapping (path = "/api/clothing", method = RequestMethod.GET )
+    @Autowired
+    WardrobeService wardrobeService;
+
+    @RequestMapping (path = "/api/clothing/all", method = RequestMethod.GET )
     public List<Clothing> getAllClothes(){
-        return WardrobeServiceImpl.get();
+        return wardrobeService.get();
     }
 
+    @RequestMapping (path = "/api/clothing/add", method = RequestMethod.POST)
+    public void addClothes(Clothing clothing) {
+        wardrobeService.add(clothing);
+    }
+
+    @RequestMapping(path = "/api/clothing/update", method = RequestMethod.PUT)
+    public void updateClothing(Clothing clothing){
+        wardrobeService.update(clothing);
+    }
+
+    @RequestMapping(path = "api/clothing/delete", method = RequestMethod.DELETE)
+    public void deleteClothes(int id){
+        wardrobeService.delete(id);
+    }
 }
