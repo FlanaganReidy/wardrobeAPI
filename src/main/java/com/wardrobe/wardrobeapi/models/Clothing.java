456e0aac9ef422d555bf5fbfef95e0c40ac2bf71
Shannon <reidy.pf@gmail.com>
2017-10-19 11:11:24 -0500

    fixed the id annotation import, tables now build

--- src/main/java/com/wardrobe/wardrobeapi/models/Clothing.java
+++ src/main/java/com/wardrobe/wardrobeapi/models/Clothing.java
@@ -3,12 +3,8 @@ package com.wardrobe.wardrobeapi.models;
 
 import com.wardrobe.wardrobeapi.Enums.Color;
 import com.wardrobe.wardrobeapi.Enums.Mood;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "clothes")
