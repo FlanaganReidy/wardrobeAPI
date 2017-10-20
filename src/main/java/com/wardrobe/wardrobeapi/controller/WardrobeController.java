6cfae71dd91e3ca2fa9fc69905d3c2344146a6ff
Shannon <reidy.pf@gmail.com>
2017-10-20 14:11:14 -0500

    enabling corse test 1

--- src/main/java/com/wardrobe/wardrobeapi/controller/WardrobeController.java
+++ src/main/java/com/wardrobe/wardrobeapi/controller/WardrobeController.java
@@ -3,14 +3,16 @@ package com.wardrobe.wardrobeapi.controller;
 import com.wardrobe.wardrobeapi.models.Clothing;
 import com.wardrobe.wardrobeapi.service.WardrobeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "https://tunic-wardrobe-api.herokuapp.com", maxAge = 3600)
 @RestController
-public class WardrobeController {
+public class WardrobeController{
 
     @Autowired
     WardrobeService wardrobeService;
