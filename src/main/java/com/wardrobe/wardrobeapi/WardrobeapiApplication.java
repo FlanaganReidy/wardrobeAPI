91a4d7d69c9c01dc5b69d53a3861a4264eca582b
Shannon <reidy.pf@gmail.com>
2017-10-30 14:10:48 -0500

    changed cors configuration

--- src/main/java/com/wardrobe/wardrobeapi/WardrobeapiApplication.java
+++ src/main/java/com/wardrobe/wardrobeapi/WardrobeapiApplication.java
@@ -3,6 +3,7 @@ package com.wardrobe.wardrobeapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,14 +14,29 @@ public class WardrobeapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WardrobeapiApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/clothing/add").allowedOrigins("http://localhost:3000");
-				registry.addMapping("/clothing/all").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurerAdapter() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/clothing/add").allowedOrigins("http://localhost:3000");
+//				registry.addMapping("/clothing/all").allowedOrigins("http://localhost:3000");
+//				registry.addMapping("/clothing/update").allowedOrigins("http://localhost:3000");
+//				registry.addMapping("/clothing/delete").allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
+	@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
 }
